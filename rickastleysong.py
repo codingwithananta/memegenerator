bb7a47a7b7539c4afe7bd2a8ae1133d440fe4e1d
Subu Kaundinya <subu.kaundinya@gmail.com>
2021-05-09 22:33:26 -0400

    I made a meme

---
+++ rickastleysong.py
@@ -0,0 +1,10 @@
+import turtle
+import os
+turtle.register_shape('https://i.imgflip.com/58x5co.gif')
+
+winsound.PlaySound('metaldrumsound.wav',winsound.SND_ASYNC)
+man = turtle.Turtle()
+man.shape = ('https://i.imgflip.com/58x5co.gif')
+man.penup
+
+turtle.done()
